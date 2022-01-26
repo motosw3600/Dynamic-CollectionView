549f9f7ff9bc8b2f7edc8f1a7ef06ee671dd688b
motosw3600 <motosw3600@gmail.com>
2022-01-26 14:38:31 +0900

    feat: CollectionView setup

--- Dynamic CollectionView/Dynamic CollectionView/ViewController.swift
+++ Dynamic CollectionView/Dynamic CollectionView/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    private let tags = ["사과", "바나나", "수박", "오렌지", "키위", "귤", "방울토마토"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    private func configureCollectionView() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    private func configureCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10
     }
 
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
 }
 
